db000c81b8ddfb1309e15abb37fde36949ee2b2b
aarushiattray <aarushiattray@gmail.com>
2025-11-14 19:23:09 -0500

    changes to index

--- index.js
+++ index.js
@@ -1,3 +1,55 @@
+// import express from "express";
+// import Hello from "./Hello.js";
+// import Lab5 from "./Lab5/index.js";
+// import cors from "cors";
+// import db from "./Kambaz/Database/index.js";
+// import UserRoutes from "./Kambaz/Users/routes.js";
+// import CourseRoutes from "./Kambaz/Courses/routes.js";
+// import ModulesRoutes from "./Kambaz/Modules/routes.js";
+// import AssignmentsRoutes from "./Kambaz/Assignments/routes.js";
+// import EnrollmentsRoutes from "./Kambaz/Enrollments/routes.js";
+// import "dotenv/config";
+// import session from "express-session";
+
+// const app = express();
+
+// app.use(
+//   cors({
+//     credentials: true,
+//     //origin: process.env.CLIENT_URL || "http://localhost:3000",
+//     origin: ['*']
+//   })
+// );
+
+// const sessionOptions = {
+//   secret: process.env.SESSION_SECRET || "kambaz",
+//   resave: false,
+//   saveUninitialized: false,
+// };
+
+// if (process.env.SERVER_ENV !== "development") {
+//   sessionOptions.proxy = true;
+//   sessionOptions.cookie = {
+//     sameSite: "none",
+//     secure: true,
+//     domain: "https://kambaz-next-js-f9a5-git-a5-aarushi-attrays-projects.vercel.app/Account/Signin",
+//   };
+// }
+
+// app.use(session(sessionOptions));
+
+// app.use(express.json());
+
+// UserRoutes(app, db);
+// CourseRoutes(app, db);
+// ModulesRoutes(app, db);
+// AssignmentsRoutes(app, db);
+// EnrollmentsRoutes(app, db);
+// Lab5(app);
+// Hello(app);
+
+// app.listen(process.env.PORT || 4000);
+
 import express from "express";
 import Hello from "./Hello.js";
 import Lab5 from "./Lab5/index.js";
@@ -10,37 +62,30 @@ import AssignmentsRoutes from "./Kambaz/Assignments/routes.js";
 import EnrollmentsRoutes from "./Kambaz/Enrollments/routes.js";
 import "dotenv/config";
 import session from "express-session";
-
+ 
 const app = express();
-const cors =require('cors');
-
+ 
 app.use(
   cors({
     credentials: true,
-    //origin: process.env.CLIENT_URL || "http://localhost:3000",
-    origin: ['*']
+    origin: '*'
   })
 );
-
 const sessionOptions = {
   secret: process.env.SESSION_SECRET || "kambaz",
   resave: false,
   saveUninitialized: false,
 };
-
 if (process.env.SERVER_ENV !== "development") {
   sessionOptions.proxy = true;
   sessionOptions.cookie = {
     sameSite: "none",
     secure: true,
-    domain: "https://kambaz-next-js-f9a5-git-a5-aarushi-attrays-projects.vercel.app/Account/Signin",
+    domain: ".vercel.app",
   };
 }
-
 app.use(session(sessionOptions));
-
 app.use(express.json());
-
 UserRoutes(app, db);
 CourseRoutes(app, db);
 ModulesRoutes(app, db);
@@ -48,5 +93,4 @@ AssignmentsRoutes(app, db);
 EnrollmentsRoutes(app, db);
 Lab5(app);
 Hello(app);
-
 app.listen(process.env.PORT || 4000);
